6201358a1abdfc7a3d6af2bde62cc69590021c1e
suhailyazdan <yazdan.suhail@gmail.com>
2023-11-21 12:39:32 +0530

    Added files via upload
    Added files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      mrdafoe:['Mr Dafoe', 'cursive'],
+      ooohbaby:['Oooh Baby', 'cursive'],
+      kaushanscript:['Kaushan Script', 'cursive'],
+      kings:['Kings', 'cursive'],
+      rocksalt:['Rock Salt', 'cursive'],
+      righteous:['Righteous', 'sans-serif'],
+      kalam:['Kalam', 'cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
